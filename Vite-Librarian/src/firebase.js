97a1248da9de5fc6628842c6f69504c2880d9e66
Youssef Shabo <youssefshabo@gmail.com>
2024-02-01 20:21:02 -0600

    ===

---
+++ Vite-Librarian/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Q3UX0bgfSgaa_4bCfhYZjEwbdMuppTA",
+  authDomain: "vite-react-librarian-6fbd2.firebaseapp.com",
+  projectId: "vite-react-librarian-6fbd2",
+  storageBucket: "vite-react-librarian-6fbd2.appspot.com",
+  messagingSenderId: "107178477815",
+  appId: "1:107178477815:web:7f334be08a101cc18cabf2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
